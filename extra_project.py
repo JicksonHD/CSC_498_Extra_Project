ad5ce54d5fee9bddee4d9b11c6f2b1aed7dafa58
JicksonHD <jacques.ghajar@lau.edu>
2023-05-08 12:52:19 +0300

    Replacing the element selector below with the actual element containing course details

--- extra_project.py
+++ extra_project.py
@@ -6,7 +6,6 @@ from getpass import getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
 username = sys.argv[1]
 password = sys.argv[2]
 
@@ -32,3 +31,21 @@ driver.get(COURSE_URL)
 html = driver.page_source
 
 soup = BeautifulSoup(html, "html.parser")
+
+# Replace the element selector below with the actual element containing course details
+course_list = soup.select("ELEMENT_SELECTOR")
+
+with open("course_offerings.csv", mode="w", newline="") as csv_file:
+    fieldnames = ["Time", "Instructor Name", "Remaining Seats"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    
+    for course in course_list:
+        # Replace the placeholder with the actual element containing course data
+        time = course.select_one("ELEMENT_SELECTOR_FOR_TIME").text
+        instructor = course.select_one("ELEMENT_SELECTOR_FOR_INSTRUCTOR").text
+        remaining_seats = course.select_one("ELEMENT_SELECTOR_FOR_REMAINING_SEATS").text
+        
+        writer.writerow({"Time": time, "Instructor Name": instructor, "Remaining Seats": remaining_seats})
+
+driver.quit()
