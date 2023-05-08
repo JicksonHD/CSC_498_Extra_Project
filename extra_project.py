c79d31ebd5758e945c6fae90472b28f6ef649599
JicksonHD <jacques.ghajar@lau.edu>
2023-05-08 16:52:52 +0300

    Remodeled the code and fixed bugs- code can now access the window we want and create csv file

--- extra_project.py
+++ extra_project.py
@@ -1,16 +1,19 @@
+import os
 import sys
-import requests
-from bs4 import BeautifulSoup
 import csv
-from getpass import getpass
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 username = sys.argv[1]
 password = sys.argv[2]
 
-LOGIN_URL = "https://myportal.lau.edu.lb/Pages/studentPortal.aspx"
-COURSE_URL = "https://myportal.lau.edu.lb/path/to/course/offering/page"
+print(username)
+print(password)
+
+LOGIN_URL = "https://myportal.lau.edu.lb/"
+TARGET_URL = "https://banweb.lau.edu.lb/prod/bwckschd.p_get_crse_unsec"
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -18,34 +21,67 @@ driver = webdriver.Chrome(options=options)
 
 driver.get(LOGIN_URL)
 
-username_input = driver.find_element_by_name("username")
+username_input = driver.find_element(By.NAME, "username")
 username_input.send_keys(username)
 
-password_input = driver.find_element_by_name("password")
+password_input = driver.find_element(By.NAME, "password")
 password_input.send_keys(password)
 
-password_input.send_keys(Keys.RETURN)
+login_button = driver.find_element(By.XPATH, "//input[@type='submit']")
+login_button.click()
 
-driver.get(COURSE_URL)
+my_courses = driver.find_element(By.XPATH, '//*[@id="zz4_TopNavigationMenuV4"]/div/ul/li/ul/li[3]/a')
+my_courses.click()
 
-html = driver.page_source
+course_offerings = driver.find_element(By.XPATH, '//*[@id="cbqwpctl00_m_g_df4e4cc9_291f_48af_90eb_524811091537"]/div/div[2]/a[4]')
+course_offerings.click()
 
-soup = BeautifulSoup(html, "html.parser")
+driver.get("https://banweb.lau.edu.lb/prod/bwckschd.p_disp_dyn_sched")
 
-# Replace the element selector below with the actual element containing course details
-course_list = soup.select("ELEMENT_SELECTOR")
+select_element = driver.find_element(By.XPATH, '//*[@id="term_input_id"]')
+select = Select(select_element)
+select.select_by_value("202410")
 
-with open("course_offerings.csv", mode="w", newline="") as csv_file:
-    fieldnames = ["Time", "Instructor Name", "Remaining Seats"]
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    
-    for course in course_list:
-        # Replace the placeholder with the actual element containing course data
-        time = course.select_one("ELEMENT_SELECTOR_FOR_TIME").text
-        instructor = course.select_one("ELEMENT_SELECTOR_FOR_INSTRUCTOR").text
-        remaining_seats = course.select_one("ELEMENT_SELECTOR_FOR_REMAINING_SEATS").text
-        
-        writer.writerow({"Time": time, "Instructor Name": instructor, "Remaining Seats": remaining_seats})
+submit_button = driver.find_element(By.XPATH, "/html/body/div[3]/form/input[2]")
+submit_button.click()
+
+subject_element = driver.find_element(By.XPATH, '//*[@id="subj_id"]')
+select = Select(subject_element)
+select.select_by_value("CSC")
+
+campus_element = driver.find_element(By.XPATH, '//*[@id="camp_id"]')
+select = Select(campus_element)
+select.select_by_value("2")
+
+course_offerings = driver.find_element(By.XPATH, '/html/body/div[3]/form/input[12]')
+course_offerings.click()
+
+elements = driver.find_elements(By.CSS_SELECTOR, ".ddtitle > a")
+names = [element.text for element in elements]
+
+elements = driver.find_elements(By.CSS_SELECTOR, "tr > .dddefault:nth-child(7)")
+instructors = [element.text for element in elements]
+
+elements = driver.find_elements(By.CSS_SELECTOR, "tr > .dddefault:nth-child(2)")
+times = [element.text for element in elements]
+
+elements = driver.find_elements(By.CSS_SELECTOR, "tr > .dddefault:nth-child(3)")
+days = [element.text for element in elements]
+
+elements = driver.find_elements(By.CSS_SELECTOR, "tr > .dddefault:nth-child(4)")
+locations = [element.text for element in elements]
+
+data = list(zip(names, instructors, locations, times, days))
+
+# Set the file path for the CSV file
+file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'course_data.csv')
+
+# Create a new CSV file and write the data to it
+with open(file_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Name", "Instructor", "Location", "Time", "Day"])
+    writer.writerows(data)
+
+print(f"CSV file saved at {file_path}")
 
 driver.quit()
